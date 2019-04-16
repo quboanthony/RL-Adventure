2493acff7cfa0081ef7c60e2f6746bb9c20952d0
Bo QU <quboanthony@gmail.com>
2019-04-16 08:10:02 +0800

    new commit

--- p1_navigation_learning_from_pixels/model.py
+++ p1_navigation_learning_from_pixels/model.py
@@ -17,16 +17,19 @@ class QNetwork(nn.Module):
         self.seed = torch.manual_seed(seed)
         "*** YOUR CODE HERE ***"
         self.cv1=nn.Conv3d(3,32,kernel_size=(1,3,3),stride=(1,3,3))
+        self.bn1 = nn.BatchNorm3d(32)
         self.cv2=nn.Conv3d(32,64,kernel_size=(1,3,3),stride=(1,3,3))
+        self.bn2 = nn.BatchNorm3d(64)
         self.cv3=nn.Conv3d(64,64,kernel_size=(1,3,3),stride=(1,3,3))
+        self.bn3= nn.BatchNorm3d(64)
         conv_out_size=self._get_conv_out_size(state_size)
         self.fc4=nn.Linear(conv_out_size,512)
         self.fc5=nn.Linear(512,action_size)
     def forward(self, state):
         """Build a network that maps state -> action values."""
-        x=F.relu(self.cv1(state))
-        x=F.relu(self.cv2(x))
-        x=F.relu(self.cv3(x))
+        x=F.relu(self.bn1(self.cv1(state)))
+        x=F.relu(self.bn2(self.cv2(x)))
+        x=F.relu(self.bn3(self.cv3(x)))
         x=x.view(x.size(0),-1)
         x=F.relu(self.fc4(x))
         return self.fc5(x)
@@ -36,9 +39,9 @@ class QNetwork(nn.Module):
 
         x = torch.rand(shape)
         
-        x=F.relu(self.cv1(x))
-        x=F.relu(self.cv2(x))
-        x=F.relu(self.cv3(x))
+        x=F.relu(self.bn1(self.cv1(x)))
+        x=F.relu(self.bn2(self.cv2(x)))
+        x=F.relu(self.bn3(self.cv3(x)))
 
         n_size = x.data.view(1, -1).size(1)
 
